5b4a488dec7d7797a79fa4bd21d9b2364105ff98
mosgizy <moshood521@gmail.com>
2023-02-24 05:13:46 +0100

    removed all dist file

--- src/app/collections/dist/pages.js
+++
@@ -1,7 +0,0 @@
-'use client';
-"use strict";
-exports.__esModule = true;
-var Collections = function () {
-    return React.createElement("section", null, "hello world");
-};
-exports["default"] = Collections;
